39da348dc84e8c6d9c0b817d272cdde50eec57f7
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2017-04-23 17:18:48 +0300

    Create main.cpp

---
+++ example/main.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+
+#include "BinarySearchTree2.hpp"
+using namespace std;
+
+int main(void)
+{
+	BinarySearchTree<unsigned int> *root = 0;
+	BinarySearchTree<unsigned int> *ptr=0;
+	return 0;
+}
